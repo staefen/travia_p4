d1ffd43fc770845e0d9611dc2af4cfa4cd4cca66
Phuong Thao Nguyen <ptng19@student.aau.dk>
2022-05-11 14:17:16 +0200

    Profile navigation again with toolbar

--- P4Travia/obj/Debug/120/android/environment.arm64-v8a.s
+++ P4Travia/obj/Debug/120/android/environment.arm64-v8a.s
@@ -85,7 +85,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"580a0e93-4d66-4d6e-938f-7c5b277e0c9b"
+	.asciz	"d26f6284-151d-4d59-b2be-f61b479b313c"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- P4Travia/obj/Debug/120/android/src/com/google/android/gms/base/R.java
+++ P4Travia/obj/Debug/120/android/src/com/google/android/gms/base/R.java
@@ -54,13 +54,13 @@ public final class R {
 	public static final class id {
 		public static final int adjust_height = 0x7f090051;
 		public static final int adjust_width = 0x7f090052;
-		public static final int auto = 0x7f09005f;
-		public static final int dark = 0x7f0900a7;
-		public static final int icon_only = 0x7f0900f9;
-		public static final int light = 0x7f090114;
-		public static final int none = 0x7f09013d;
-		public static final int standard = 0x7f0901b4;
-		public static final int wide = 0x7f0901f6;
+		public static final int auto = 0x7f090061;
+		public static final int dark = 0x7f0900ad;
+		public static final int icon_only = 0x7f090100;
+		public static final int light = 0x7f09011b;
+		public static final int none = 0x7f090145;
+		public static final int standard = 0x7f0901c1;
+		public static final int wide = 0x7f090206;
 	}
 	public static final class string {
 		public static final int common_google_play_services_enable_button = 0x7f0f0033;
