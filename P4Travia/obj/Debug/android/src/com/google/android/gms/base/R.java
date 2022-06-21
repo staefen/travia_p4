e894f086e2041076a800728bc2fce387b945d1a3
MariaKristineTommer <mtamme20@student.aau.dk>
2022-06-21 11:23:58 +0200

    siste commit til stephen

--- P4Travia/obj/Debug/android/src/com/google/android/gms/base/R.java
+++ P4Travia/obj/Debug/android/src/com/google/android/gms/base/R.java
@@ -56,11 +56,11 @@ public final class R {
 		public static final int adjust_width = 0x7f090056;
 		public static final int auto = 0x7f090065;
 		public static final int dark = 0x7f0900b5;
-		public static final int icon_only = 0x7f090113;
-		public static final int light = 0x7f09012c;
-		public static final int none = 0x7f09015a;
-		public static final int standard = 0x7f0901d6;
-		public static final int wide = 0x7f090223;
+		public static final int icon_only = 0x7f090114;
+		public static final int light = 0x7f09012d;
+		public static final int none = 0x7f09015b;
+		public static final int standard = 0x7f0901d7;
+		public static final int wide = 0x7f090224;
 	}
 	public static final class string {
 		public static final int common_google_play_services_enable_button = 0x7f0f0035;
